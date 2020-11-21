96c77c1a92509265ce69226e88db5ae66621f28b
Ritwick <rchaudhr@andrew.cmu.edu>
2020-11-20 21:02:18 -0500

    Adding custom transform for cropping

---
+++ VizWiz-VQA-PyTorch/datasets/custom_transforms.py
@@ -0,0 +1,96 @@
+import math
+import torch
+import torchvision
+import torch.functional as F
+
+class RandomResizedCrop(torch.nn.Module):
+    """Crop the given image to random size and aspect ratio.
+    The image can be a PIL Image or a Tensor, in which case it is expected
+    to have [..., H, W] shape, where ... means an arbitrary number of leading dimensions
+
+    A crop of random size (default: of 0.08 to 1.0) of the original size and a random
+    aspect ratio (default: of 3/4 to 4/3) of the original aspect ratio is made. This crop
+    is finally resized to given size.
+    This is popularly used to train the Inception networks.
+
+    Args:
+        size (int or sequence): expected output size of each edge. If size is an
+            int instead of sequence like (h, w), the longer side is resized to that. 
+            If provided a tuple or list of length 1, it will be interpreted as (size[0], size[0]).
+        scale (tuple of float): range of size of the origin size cropped
+        ratio (tuple of float): range of aspect ratio of the origin aspect ratio cropped.
+        interpolation (int): Desired interpolation enum defined by `filters`_.
+            Default is ``PIL.Image.BILINEAR``. If input is Tensor, only ``PIL.Image.NEAREST``, ``PIL.Image.BILINEAR``
+            and ``PIL.Image.BICUBIC`` are supported.
+    """
+
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+        super().__init__()
+        '''
+        Modified from PyTorch to remove the size from being a tuple. 
+        This will allow non-square outputs with integer sizes
+        '''
+        self.size = size
+        self.interpolation = interpolation
+        self.scale = scale
+        self.ratio = ratio
+
+    @staticmethod
+    def get_params(
+            img: Tensor, scale: List[float], ratio: List[float]
+    ) -> Tuple[int, int, int, int]:
+        """Get parameters for ``crop`` for a random sized crop.
+
+        Args:
+            img (PIL Image or Tensor): Input image.
+            scale (list): range of scale of the origin size cropped
+            ratio (list): range of aspect ratio of the origin aspect ratio cropped
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+                sized crop.
+        """
+        width, height = F._get_image_size(img)
+        area = height * width
+
+        for _ in range(10):
+            target_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
+            log_ratio = torch.log(torch.tensor(ratio))
+            aspect_ratio = torch.exp(
+                torch.empty(1).uniform_(log_ratio[0], log_ratio[1])
+            ).item()
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if 0 < w <= width and 0 < h <= height:
+                i = torch.randint(0, height - h + 1, size=(1,)).item()
+                j = torch.randint(0, width - w + 1, size=(1,)).item()
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = float(width) / float(height)
+        if in_ratio < min(ratio):
+            w = width
+            h = int(round(w / min(ratio)))
+        elif in_ratio > max(ratio):
+            h = height
+            w = int(round(h * max(ratio)))
+        else:  # whole image
+            w = width
+            h = height
+        i = (height - h) // 2
+        j = (width - w) // 2
+        return i, j, h, w
+
+
+    def forward(self, img):
+        """
+        Args:
+            img (PIL Image or Tensor): Image to be cropped and resized.
+
+        Returns:
+            PIL Image or Tensor: Randomly cropped and resized image.
+        """
+        i, j, h, w = self.get_params(img, self.scale, self.ratio)
+        return F.resized_crop(img, i, j, h, w, self.size, self.interpolation)
--- VizWiz-VQA-PyTorch/preprocessing/images.py
+++ VizWiz-VQA-PyTorch/preprocessing/images.py
@@ -4,64 +4,82 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from PIL import Image
 
+from datasets.custom_transforms import RandomResizedCrop
+
 IMG_EXTENSIONS = [
-    '.jpg', '.JPG', '.jpeg', '.JPEG',
-    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+	'.jpg', '.JPG', '.jpeg', '.JPEG',
+	'.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
 ]
 
 
 def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+	return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+
 
 
 class ImageDataset(data.Dataset):
 
-    def __init__(self, path, transform=None):
-        self.path = path
-        self.transform = transform
+	def __init__(self, path, transform=None):
+		self.path = path
+		self.transform = transform
 
-        # Load the paths to the images available in the folder
-        self.image_names = self._load_img_paths()
+		# Load the paths to the images available in the folder
+		self.image_names = self._load_img_paths()
 
-        if len(self.image_names) == 0:
-            raise (RuntimeError("Found 0 images in " + path + "\n"
-                                                              "Supported image extensions are: " + ",".join(
-                IMG_EXTENSIONS)))
-        else:
-            print('Found {} images in {}'.format(len(self), self.path))
+		if len(self.image_names) == 0:
+			raise (RuntimeError("Found 0 images in " + path + "\n"
+															  "Supported image extensions are: " + ",".join(
+				IMG_EXTENSIONS)))
+		else:
+			print('Found {} images in {}'.format(len(self), self.path))
 
-    def __getitem__(self, index):
-        item = {}
-        item['name'] = self.image_names[index]
-        item['path'] = os.path.join(self.path, item['name'])
+	def __getitem__(self, index):
+		item = {}
+		item['name'] = self.image_names[index]
+		item['path'] = os.path.join(self.path, item['name'])
 
-        # Use PIL to load the image
-        item['visual'] = Image.open(item['path']).convert('RGB')
-        if self.transform is not None:
-            item['visual'] = self.transform(item['visual'])
+		# Use PIL to load the image
+		item['visual'] = Image.open(item['path']).convert('RGB')
+		if self.transform is not None:
+			item['visual'] = self.transform(item['visual'])
 
-        return item
+		return item
 
-    @property
-    def get_image_names(self):
-        return self.image_names
+	@property
+	def get_image_names(self):
+		return self.image_names
 
-    def __len__(self):
-        return len(self.image_names)
+	def __len__(self):
+		return len(self.image_names)
 
-    def _load_img_paths(self):
-        images = []
-        for name in os.listdir(self.path):
-            if is_image_file(name):
-                images.append(name)
-        return images
+	def _load_img_paths(self):
+		images = []
+		for name in os.listdir(self.path):
+			if is_image_file(name):
+				images.append(name)
+		return images
 
 
-def get_transform(img_size):
-    return transforms.Compose([
-        transforms.Resize(img_size),
-        transforms.CenterCrop(img_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4774, 0.4510, 0.4103],
-                             std=[0.2741, 0.2692, 0.2841]),
-    ])
+def get_transform(config):
+	img_config = config['images']
+	img_size = img_config['img_size']
+	all_transforms = [
+		transforms.Resize(img_size),
+		transforms.CenterCrop(img_size)
+	]
+	if img_config['augmentation']['do_crop']:
+		all_transforms.append(
+			# transforms.RandomResizedCrop( 
+			RandomResizedCrop(
+				img_size,
+				scale = img_config['augmentation']['crop_scale'],
+				aspect_ratio = img_config['augmentation']['crop_ratio']
+			))
+	all_transforms += [
+		transforms.ToTensor(),
+		transforms.Normalize(mean=[0.4774, 0.4510, 0.4103],
+							std=[0.2741, 0.2692, 0.2841]),
+	]
+	return transforms.Compose(all_transforms)
